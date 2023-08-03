281a77fc26565da7c136723dc8e6a8696a965250
CodeConnects <codeconnects@gmail.com>
2023-08-03 17:05:54 -0400

    add basic html piano layout

--- src/App.js
+++ src/App.js
@@ -6,6 +6,27 @@ function App() {
       <header className="App-header">
         <p>Piano</p>
       </header>
+      <div className='piano'>
+
+        <div className='piano-upper'>
+
+          <div className='piano-speaker'></div>
+
+          <div className='piano-controls'>
+            <div className='piano-control-label'>Octave</div>
+            <input type='range' min='0' max='100' value='100' />
+          </div>
+
+          <div className='piano-speaker'></div>
+        </div>
+
+        <div className='piano-keyboard'>
+          <div className='white-key'></div>
+          <div className='black-key'></div>
+          <div className='white-key'></div>
+          <div className='black-key'></div>
+        </div>
+      </div>
     </div>
   );
 }
