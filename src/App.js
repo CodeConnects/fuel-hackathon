e87fda229cd267428b62f7f9f4a7fa5893cc9b86
CodeConnects <codeconnects@gmail.com>
2023-10-04 19:43:00 -0400

    remove synth call and change PianoOctave rendering

--- src/App.js
+++ src/App.js
@@ -2,39 +2,33 @@
 // It renders the Speakers and Controls components, which are displayed above the keyboard.
 // It also renders the PianoOctave component, which renders the piano keys for each octave.
 
-import * as Tone from 'tone';
-
 import PianoSpeaker from './components/PianoSpeaker';
 import PianoControls from './components/PianoControls';
 import PianoOctave from './components/PianoOctave';
 import './App.css';
 
-const synth = new Tone.Synth().toDestination();
-
 const App = () => {
-  const octaves = 5;
-  const pianoKeys = [];
-
-  for (let i = 0; i < octaves; i++) {
-    pianoKeys.push(<PianoOctave key={i} oct={i+1} />);
-  }
+  const NUM_OCTAVES = 5;
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+
+      <header className="app-header">
         <p>Piano</p>
       </header>
-      
+
       <div className='piano'>
 
         <div className='piano-upper'>
           <PianoSpeaker />
-          <PianoControls synth={synth} />
+          <PianoControls />
           <PianoSpeaker />
         </div>
 
         <div className='piano-wrap'>
-          {pianoKeys}
+          {Array.from({ length: NUM_OCTAVES }, (_, i) => (
+            <PianoOctave key={i} octave={i + 1} />
+          ))}
         </div>
 
       </div>
