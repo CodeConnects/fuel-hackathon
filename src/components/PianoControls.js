e61327c5b32ff13127d29792e823d48aacab05ed
CodeConnects <codeconnects@gmail.com>
2023-10-04 19:24:41 -0400

    build out start, mute and volume controls

--- src/components/PianoControls.js
+++ src/components/PianoControls.js
@@ -1,9 +1,42 @@
+import { useState } from 'react';
+import * as Tone from 'tone';
 import '../styles/PianoControls.css';
 
 const PianoControls = () => {
+  
+  const [mute, setMute] = useState(true);
+  const [volume, setVolume] = useState(-Infinity);
+
+  const toggleMute = () => {
+    Tone.Destination.mute = !mute;
+    setMute(!mute);
+  }
+
+  const handleStart = () => {
+    Tone.start();
+    setVolume(-30); // half of max
+    Tone.Destination.volume.value = volume;
+    setMute(false);
+  };
+
+  const changeVolume = (e) => {
+    const newVolume = e.target.value;
+    setVolume(newVolume);
+    Tone.Destination.volume.value = newVolume;
+  };
+
   return (
     <div className="piano-controls">
-      controls
+      <button className='piano-start' onClick={ handleStart }>Start</button>
+      <button className='piano-mute' onClick={ toggleMute }>{mute ? 'Unmute' : 'Mute'}</button>
+      <input className='piano-volume' 
+        type='range' 
+        min='-60' 
+        max='0' 
+        step='1'
+        value={volume}
+        onChange={ changeVolume } 
+      />
     </div>
   );
 }
