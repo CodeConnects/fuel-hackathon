0bed71268fd2f2bc9de7675c87585f7acc64b30f
CodeConnects <codeconnects@gmail.com>
2023-10-04 19:40:24 -0400

    move new synth call into this component and put keys in an array with map to render

--- src/components/PianoOctave.js
+++ src/components/PianoOctave.js
@@ -1,34 +1,39 @@
 // This component renders a single octave of a piano keyboard.
 // It consists of 12 keys: 7 white keys and 5 black keys.
-// Each key has an onClick event handler that calls handleClick(note)
-// with the note name as an argument.
+// Each key has an onClick function that triggers a note on the synth
 // Tone.js is used to play the note.
 // Synth is a Tone.js instrument that plays a note when triggered.
 
+import { Synth } from "tone";
+
 import '../styles/PianoOctave.css';
 
-const PianoOctave = (props) => {
+const synth = new Synth().toDestination();
 
-  // This function plays a note of the given frequency
-  // for an eighth note.
-  const playNote = (note) => {
-    props.synth.triggerAttackRelease(note + props.oct, '8n');
-  }
+const whiteKeys = ["C", "D", "E", "F", "G", "A", "B"];
+const blackKeys = ["C#", "D#", "F#", "G#", "A#"];
 
+function PianoOctave({ octave }) {
   return (
     <div className="piano-octave">
-      <div className="key white" onClick={() => playNote('C')}></div>
-      <div className="key black" onClick={() => playNote('C#')}></div>
-      <div className="key white" onClick={() => playNote('D')}></div>
-      <div className="key black" onClick={() => playNote('D#')}></div>
-      <div className="key white" onClick={() => playNote('E')}></div>
-      <div className="key white" onClick={() => playNote('F')}></div>
-      <div className="key black" onClick={() => playNote('F#')}></div>
-      <div className="key white" onClick={() => playNote('G')}></div>
-      <div className="key black" onClick={() => playNote('G#')}></div>
-      <div className="key white" onClick={() => playNote('A')}></div>
-      <div className="key black" onClick={() => playNote('A#')}></div>
-      <div className="key white" onClick={() => playNote('B')}></div>
+      {whiteKeys.map(note => (
+        <div
+          key={note}
+          className="white-key"
+          onClick={() => synth.triggerAttackRelease(`${note}${octave}`, "8n")}
+        >
+          {note}{octave}
+        </div>
+      ))}
+      {blackKeys.map(note => (
+        <div
+          key={note}
+          className="black-key"
+          onClick={() => synth.triggerAttackRelease(`${note}${octave}`, "8n")}
+        >
+          {note}{octave}
+        </div>
+      ))}
     </div>
   );
 }
