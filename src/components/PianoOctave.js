d1c9cc11e65a71ad5d5bbb1a52f9250faca177f1
CodeConnects <codeconnects@gmail.com>
2023-10-04 19:24:21 -0400

    add Tone.js synth to play the notes on click

--- src/components/PianoOctave.js
+++ src/components/PianoOctave.js
@@ -1,32 +1,34 @@
-
 // This component renders a single octave of a piano keyboard.
 // It consists of 12 keys: 7 white keys and 5 black keys.
 // Each key has an onClick event handler that calls handleClick(note)
 // with the note name as an argument.
+// Tone.js is used to play the note.
+// Synth is a Tone.js instrument that plays a note when triggered.
 
 import '../styles/PianoOctave.css';
 
-const PianoOctave = () => {
+const PianoOctave = (props) => {
 
-  const handleClick = (note) => {
-    console.log(`Played note: ${note}`);
-    // Todo: add logic to play the note sound
+  // This function plays a note of the given frequency
+  // for an eighth note.
+  const playNote = (note) => {
+    props.synth.triggerAttackRelease(note + props.oct, '8n');
   }
 
   return (
     <div className="piano-octave">
-      <div className="key white" onClick={() => handleClick('C')}></div>
-      <div className="key black" onClick={() => handleClick('C#')}></div>
-      <div className="key white" onClick={() => handleClick('D')}></div>
-      <div className="key black" onClick={() => handleClick('D#')}></div>
-      <div className="key white" onClick={() => handleClick('E')}></div>
-      <div className="key white" onClick={() => handleClick('F')}></div>
-      <div className="key black" onClick={() => handleClick('F#')}></div>
-      <div className="key white" onClick={() => handleClick('G')}></div>
-      <div className="key black" onClick={() => handleClick('G#')}></div>
-      <div className="key white" onClick={() => handleClick('A')}></div>
-      <div className="key black" onClick={() => handleClick('A#')}></div>
-      <div className="key white" onClick={() => handleClick('B')}></div>
+      <div className="key white" onClick={() => playNote('C')}></div>
+      <div className="key black" onClick={() => playNote('C#')}></div>
+      <div className="key white" onClick={() => playNote('D')}></div>
+      <div className="key black" onClick={() => playNote('D#')}></div>
+      <div className="key white" onClick={() => playNote('E')}></div>
+      <div className="key white" onClick={() => playNote('F')}></div>
+      <div className="key black" onClick={() => playNote('F#')}></div>
+      <div className="key white" onClick={() => playNote('G')}></div>
+      <div className="key black" onClick={() => playNote('G#')}></div>
+      <div className="key white" onClick={() => playNote('A')}></div>
+      <div className="key black" onClick={() => playNote('A#')}></div>
+      <div className="key white" onClick={() => playNote('B')}></div>
     </div>
   );
 }
