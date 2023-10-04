985b3eb9741c9b9cdbc1256c6a003026ee56d778
CodeConnects <codeconnects@gmail.com>
2023-10-04 15:50:08 -0400

    add initial descriptive comments to the main app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+// This App component is the root of the React application and displays a piano keyboard.
+// It renders the Speakers and Controls components, which are displayed above the keyboard.
+// It also renders the PianoOctave component, which renders the piano keys for each octave.
+
 import PianoSpeaker from './components/PianoSpeaker';
 import PianoControls from './components/PianoControls';
 import PianoOctave from './components/PianoOctave';
 import './App.css';
 
-function App() {
+const App = () => {
   const octaves = 5;
   const pianoKeys = [];
 
@@ -16,6 +20,7 @@ function App() {
       <header className="App-header">
         <p>Piano</p>
       </header>
+      
       <div className='piano'>
 
         <div className='piano-upper'>
