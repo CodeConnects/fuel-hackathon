a83b40996483d3ed4ccac4bcdd9b1b3fcd798a9f
CodeConnects <codeconnects@gmail.com>
2023-10-04 22:02:44 -0400

    expand octave component jsx

--- src/components/PianoOctave.js
+++ src/components/PianoOctave.js
@@ -14,26 +14,45 @@ const whiteKeys = ["C", "D", "E", "F", "G", "A", "B"];
 const blackKeys = ["C#", "D#", "F#", "G#", "A#"];
 
 function PianoOctave({ octave }) {
+
   return (
     <div className="piano-octave">
-      {whiteKeys.map(note => (
-        <div
-          key={note}
-          className="white-key"
-          onClick={() => synth.triggerAttackRelease(`${note}${octave}`, "8n")}
-        >
-          {note}{octave}
-        </div>
-      ))}
-      {blackKeys.map(note => (
-        <div
-          key={note}
-          className="black-key"
-          onClick={() => synth.triggerAttackRelease(`${note}${octave}`, "8n")}
-        >
-          {note}{octave}
-        </div>
-      ))}
+      <div className="key white" onClick={() => synth.triggerAttackRelease(`${whiteKeys[0]}${octave}`, "8n")}>
+        {whiteKeys[0]}{octave}
+      </div>
+      <div className="key black" onClick={() => synth.triggerAttackRelease(`${blackKeys[0]}${octave}`, "8n")}>
+        {blackKeys[0]}{octave}
+      </div>
+      <div className="key white" onClick={() => synth.triggerAttackRelease(`${whiteKeys[1]}${octave}`, "8n")}>
+        {whiteKeys[1]}{octave}
+      </div>
+      <div className="key black" onClick={() => synth.triggerAttackRelease(`${blackKeys[1]}${octave}`, "8n")}>
+        {blackKeys[1]}{octave}
+      </div>
+      <div className="key white" onClick={() => synth.triggerAttackRelease(`${whiteKeys[2]}${octave}`, "8n")}>
+        {whiteKeys[2]}{octave}
+      </div>
+      <div className="key white" onClick={() => synth.triggerAttackRelease(`${whiteKeys[3]}${octave}`, "8n")}>
+        {whiteKeys[3]}{octave}
+      </div>
+      <div className="key black" onClick={() => synth.triggerAttackRelease(`${blackKeys[2]}${octave}`, "8n")}>
+        {blackKeys[2]}{octave}
+      </div>
+      <div className="key white" onClick={() => synth.triggerAttackRelease(`${whiteKeys[4]}${octave}`, "8n")}>
+        {whiteKeys[4]}{octave}
+      </div>
+      <div className="key black" onClick={() => synth.triggerAttackRelease(`${blackKeys[3]}${octave}`, "8n")}>
+        {blackKeys[3]}{octave}
+      </div>
+      <div className="key white" onClick={() => synth.triggerAttackRelease(`${whiteKeys[5]}${octave}`, "8n")}>
+        {whiteKeys[5]}{octave}
+      </div>
+      <div className="key black" onClick={() => synth.triggerAttackRelease(`${blackKeys[4]}${octave}`, "8n")}>
+        {blackKeys[4]}{octave}
+      </div>
+      <div className="key white" onClick={() => synth.triggerAttackRelease(`${whiteKeys[6]}${octave}`, "8n")}>
+        {whiteKeys[6]}{octave}
+      </div>
     </div>
   );
 }
