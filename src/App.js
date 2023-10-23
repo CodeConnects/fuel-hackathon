27eb5e7df5d6b19bb4eec477f371e07f07558602
CodeConnects <codeconnects@gmail.com>
2023-10-23 12:20:15 -0400

    add page title fade effect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 // This App component is the root of the React application and displays a piano keyboard.
 
+import React, { useEffect } from 'react';
 import Piano from './components/Piano';
 import './App.css';
 
 const App = () => {
+  useEffect(() => {
+    const fadeTime = setTimeout(() => {
+      const element = document.getElementById("opening-title");
+      if (element) {
+        element.classList.add("fade-out");
+      }
+    }, 5000);
+
+    return () => {
+      clearTimeout(fadeTime);
+    };
+  }, []);
+
   return (
     <div className="app">
 
       <header className="app-header">
-        <p>Piano</p>
+        <h1 id="opening-title">7 Octave Piano Synthesizer</h1>
       </header>
 
       <Piano />
